5114f7d60822b0999b548eb8cfddadbf0d96fa2e
Maksim Dubovyk <maksimdubovyk2004@gmail.com>
2024-05-31 12:58:30 +0300

    add models

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    def __iter__(self):
+        yield 'name', self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return self.title
